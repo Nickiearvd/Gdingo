0377a42c10938757526cfa22cc03e84137211ba1
annadarner <darner.96@hotmail.com>
2017-10-15 19:00:17 +0200

    created file

--- Index.php
+++ Index.php
@@ -1,1 +1,13 @@
+<!Doctype html>
 <html>
+	<head>
+		<title>Index</title>
+			<Meta name="description" content="my page"/>
+			<Meta charset="utf-8"/>
+			<link rel="stylesheet" href="main.css" type="text/css" />
+			<?php include 'config.php';?>
+	</head>
+	<body>
+		<?php include 'header.php';?>
+	</body>
+</html>
