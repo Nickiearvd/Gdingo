3bfd630b25044525ed2e5886da8e08f4ebd3147a
Nickiearvd <arvidssonnickie@gmail.com>
2017-10-16 17:54:29 +0200

    n

--- finddrinks.php
+++ finddrinks.php
@@ -9,5 +9,22 @@
 			<?php include 'config.php';?>
 	</head>
 	<body>
+		<div class='container'>
+
+			<div class="containersearch">
+				<form action="finddrinks.php" method="POST">
+					<div class='namedrink'>
+						<h3>Search after a drinkname</h3>
+						<input type="text" id="name" name="searchname" placeholder="Search after a drink name">
+						<input class="button" type="submit" name="submit" value="Search">
+					</div>
+				    <input type="text" id="ing1" name="searching" placeholder="Search after one ingrediens ">
+				    <input type="text" id="ing2" name="searching" placeholder="Search after one ingrediens ">
+
+				    <input class="button" type="submit" name="submit" value="Search">
+		  		</form>
+			</div>
+
+		</div>
 	</body>
 </html>
