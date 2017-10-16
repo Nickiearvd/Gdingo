49e252436a804af92f2db769a1b3d5adea0088e6
annadarner <darner.96@hotmail.com>
2017-10-16 17:54:43 +0200

    inspo

--- Index.php
+++ Index.php
@@ -10,53 +10,80 @@
 	</head>
 	<body>
 
-	<div id="c"></div>
+		<div id="c"></div> <!-- For the menu -->
 
 		<!-- https://www.w3schools.com/w3css/w3css_slideshow.asp 
 		The basic to the slideshow is taken from w3schools -->
 		<div id="content">
 
-			<div id="intro">
+			<div id="intro"> <!-- intro starts -->
 				<h1>In search for a drink?</h1>
 				<p class="introp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et maximus augue. Vivamus vel dolor nisi. Quisque molestie scelerisque diam nec semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br> Nunc ac mollis orci. Maecenas ac volutpat massa. Nunc et ligula accumsan, condimentum lectus nec, fringilla nisl.
 				Sed elit elit, auctor non sollicitudin ultrices, semper vitae nisi. Proin pharetra erat nisi, in tristique nisl sagittis et. Nullam iaculis imperdiet condimentum. Donec imperdiet commodo aliquam. Vestibulum eleifend est justo, quis lacinia libero blandit at. Aliquam vehicula nisl sed nulla gravida maximus. In suscipit a nulla et suscipit. </p>
 				<button class="introbtn"> FIND OUT MORE </button>
-			</div>
-			<img src="Images/tri.png" class="tri">
-			<div id="firstc">
-			
+			</div> <!-- intro ends -->
+
+			<img src="Images/tri.png" class="tri"> <!-- triangle -->
+
+			<div id="firstc"> <!-- container with about + top starts -->			
 				<h2>About<br>us</h2>
-				<div id="about">
+				<div id="about"> <!-- about us div -->
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et maximus augue. Vivamus vel dolor nisi. Quisque molestie scelerisque diam nec semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br> Nunc ac mollis orci. Maecenas ac volutpat massa. Nunc et ligula accumsan, condimentum lectus nec, fringilla nisl.
 					Sed elit elit, auctor non sollicitudin ultrices, semper vitae nisi. Proin pharetra erat nisi, in tristique nisl sagittis et. Nullam iaculis imperdiet condimentum. Donec imperdiet commodo aliquam. Vestibulum eleifend est justo, quis lacinia libero blandit at. Aliquam vehicula nisl sed nulla gravida maximus. In suscipit a nulla et suscipit. <br> <br>
 					Sed elit elit, auctor non sollicitudin ultrices, semper vitae nisi. Proin pharetra erat nisi, in tristique nisl sagittis et. Nullam iaculis imperdiet condimentum. Donec imperdiet commodo aliquam. Vestibulum eleifend est justo, quis lacinia libero blandit at. Aliquam vehicula nisl sed nulla gravida maximus. In suscipit a nulla et suscipit. </p>
 				</div>
-				<div id="slideshow">
-					<div class="mySlides">
+				<div id="slideshow"> <!-- slideshow -->
+					<div class="mySlides"> <!-- first slide -->
 						<h3>TOP 10</h3>
 						<img class="slideimg" src="Images/raspberry-mojito.jpg">
 						<h5>Raspberry fruit Mojito</h5>
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare dignissim nisl a convallis. Fusce ac purus quis sapien mollis blandit vel id velit. Duis eget maximus purus. </p>
 					</div>
-					<div class="mySlides">
+
+					<div class="mySlides"> <!-- second slide -->
 					<h3>TOP 10</h3>
 					<img class="slideimg" src="Images/cheescake-smoothie.jpg">
 					<h5>Cheescake smoothie drink</h5>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare dignissim nisl a convallis. Fusce ac purus quis sapien mollis blandit vel id velit. Duis eget maximus purus.</p>
 					</div>
-					<div class="mySlides">
+
+					<div class="mySlides"> <!-- third slide -->
 					<h3>TOP 10</h3>
 					<img class="slideimg" src="Images/passion-mojito.jpg">
 					<h5>Passion fruit Mojito</h5>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare dignissim nisl a convallis. Fusce ac purus quis sapien mollis blandit vel id velit. Duis eget maximus purus. </p>
 					</div>
-						<button class="slidebtn1" onclick="plusDivs(-1)">&#10094;</button>
+
+						<button class="slidebtn1" onclick="plusDivs(-1)">&#10094;</button> <!-- buttons for the slide -->
 						<button class="slidebtn2" onclick="plusDivs(+1)">&#10095;</button>
+
+				</div>
+				<div id="test"></div> <!-- the green border -->
+			</div> <!-- firstc ends-->
+
+			<img src="Images/tri3.png" class="tri2"> <!-- triangle -->
+			
+			<div id="gallery"> <!-- gallery div -->
+				<div id="gallerycol">
+
+					<h2> <font color="#12e696">INSPIRATION </font></h2>
+					<?php
+						$files = glob("images/inspo/*.*");
+
+						for ($i=0; $i<count($files); $i++) {
+						    $image = $files[$i];
+						    echo '<img src="'.$image .'" alt="Random image" class="galleryimg" />'."<br /><br />";
+						}
+
+					?>
 				</div>
 			</div>
-		</div>
-		<script>
-			var slideIndex = 1;
+
+		</div> <!-- content div ends -->
+
+		<script> 
+			// Script for slideshow
+			var slideIndex = 1; 
 			showDivs(slideIndex);
 
 			function plusDivs(n) {
