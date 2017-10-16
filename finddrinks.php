0b7e0707d42da9f7fdc42ae5826548775aba3096
Nickiearvd <arvidssonnickie@gmail.com>
2017-10-16 16:58:17 +0200

    New files

---
+++ finddrinks.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+<?php include 'header.php';?>
+	<head>
+		<title>Index</title>
+			<Meta name="description" content="my page"/>
+			<Meta charset="utf-8"/>
+			<link rel="stylesheet" href="mainfinddrinks.css" type="text/css" />
+			<?php include 'config.php';?>
+	</head>
+	<body>
+	</body>
+</html>
