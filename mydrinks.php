c5f14f647ec8d1bda9f8cfc7efef02d0831ebb61
Julia <julia.petersson@users.noreply.github.com>
2017-10-24 22:13:21 +0200

    aa

--- mydrinks.php
+++ mydrinks.php
@@ -1,32 +1,74 @@
+<?php include 'config.php';?>
 <!Doctype html>
 <html>
+<?php include 'header.php';?>
 	<head>
 		<title>Index</title>
 			<Meta name="description" content="my page"/>
 			<Meta charset="utf-8"/>
-			<link rel="stylesheet" href="main.css" type="text/css" />
-			<?php include 'config.php';?>
+			<link rel="stylesheet" href="mydrinks.css" type="text/css" />
 	</head>
 	<body>
-		<?php include 'header.php';?>
-		<div class='container'>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in libero eget massa feugiat tristique. Duis vel erat rutrum, sagittis odio ac, consectetur elit. Quisque malesuada sit amet est ac luctus. Quisque eget placerat lacus, non tincidunt turpis. Suspendisse scelerisque, tellus gravida faucibus egestas, purus risus dictum sapien, eget vulputate leo tortor vitae augue. Phasellus suscipit neque a diam scelerisque dignissim. Suspendisse nulla urna, interdum non aliquet in, vestibulum ut neque. Pellentesque vel tortor aliquam, rhoncus dui sed, egestas est. Vivamus elit sapien, porta et quam maximus, ultricies dapibus justo. Aenean commodo eleifend justo, eu venenatis libero condimentum sed. Etiam ut mi iaculis, laoreet ex sed, ultricies libero. Etiam eleifend ipsum in mi luctus, non mattis dui consequat. Vestibulum eros est, venenatis sed tempor ac, euismod non ipsum. Duis sed metus nisi.
 
-Phasellus bibendum leo felis, sit amet vulputate odio rhoncus ut. Suspendisse ut pulvinar diam. Ut egestas orci eget varius cursus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas ultrices quam non arcu consequat, eget ultrices purus hendrerit. Aliquam est sapien, consectetur pretium ultricies vitae, venenatis et odio. Etiam dignissim vel felis sit amet ultricies. Nam in venenatis tortor. Vivamus ut dictum tellus, eget sodales dui. Quisque sagittis diam non dolor imperdiet imperdiet.
+		<div id="c"></div> <!-- For the menu -->
 
-Fusce nec sapien non neque mollis sodales. Aenean sit amet metus arcu. Nullam fermentum massa eu tellus ornare, euismod dictum eros viverra. Nunc eu ligula diam. Cras vestibulum, risus non finibus bibendum, elit ligula dapibus lorem, sed dignissim libero dolor vitae sapien. Praesent in feugiat est. Pellentesque sagittis nisl non laoreet condimentum. Morbi maximus eros velit, sed aliquam sem pulvinar ac. Pellentesque mattis ac quam ut viverra. Cras ac ultricies nisi, at aliquam elit. Vivamus eros velit, scelerisque id lacus commodo, hendrerit euismod purus. Nullam venenatis dui quis dui ornare, laoreet finibus quam lobortis.
+		<!-- https://www.w3schools.com/w3css/w3css_slideshow.asp 
+		The basic to the slideshow is taken from w3schools -->
+		<div id="content">
 
-Suspendisse dignissim tortor in ligula porta, at efficitur dolor aliquam. Duis in ante pharetra, faucibus purus ac, euismod sem. Aliquam imperdiet dui sit amet magna cursus, sit amet placerat nunc interdum. In et facilisis est. Etiam sed ex a est egestas luctus. Quisque bibendum sapien a tortor dictum vulputate. Donec lacinia lacinia leo ac euismod. Curabitur fringilla est sem, ac lobortis diam molestie at. Fusce semper euismod pretium. Morbi nec eros vitae magna pharetra blandit sed nec lectus. Praesent id ultricies massa. Donec egestas eros non ex ullamcorper, viverra tincidunt enim condimentum. Nunc quis aliquet felis, nec bibendum arcu. Morbi nec orci laoreet, dapibus lectus et, elementum justo. Praesent eu dapibus purus, sed placerat lorem. Suspendisse hendrerit non neque vel fermentum.
+			<div id="intro"> <!-- intro starts -->
+				<h1>MY DRINKING SHELF</h1>
+				
+				<button class="introbtn"> SEARCH </button>
+			</div> <!-- intro ends -->
 
-Suspendisse at mollis magna, aliquet molestie sapien. Donec sed blandit velit, eget varius erat. Sed tincidunt mauris et est malesuada, id volutpat risus vehicula. In tristique tempor vehicula. Integer pharetra fermentum nunc non iaculis. Nullam blandit nec magna quis dapibus. Phasellus pharetra justo ut mi tempus venenatis. Nulla dolor neque, tempor nec auctor non, congue ut augue. Duis fermentum pellentesque est nec volutpat. Suspendisse aliquet tellus mauris, et dictum ligula commodo ac. Ut sapien sem, bibendum vitae dictum nec, eleifend at lacus.
+			<img src="Images/tri.png" class="tri"> <!-- triangle -->
 
-Generated 5 paragraphs, 507 words, 3429 bytes of Lorem Ipsum
-help@lipsum.com
-		</div>
-		<style>
-			.container{
-				padding-top:100px;
-			}
-		</style>
+			<div id="firstc"> <!-- black container starts -->			
+				
+				<div class="drinkbox">	
+				<div class="bildhalva">
+					<img class="thumbnail" src="Images/passion-mojito.jpg">
+					<div class="rating">
+						<img class="ratinglogo" src="Images/logotype2.png">
+						<img class="ratinglogo" src="Images/logotype2.png">
+						<img class="ratinglogo" src="Images/logotype2.png">
+						<img class="ratinglogo" src="Images/logotype2.png">
+						<img class="ratinglogo" src="Images/logotype2.png">
+					</div>
+				</div>		
+				<div class="texthalva">
+					<h2>TEQUILA</h2>
+					<p class="recept">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et maximus augue. Vivamus vel dolor nisi. Quisque molestie scelerisque diam nec semper. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br><br> Nunc ac mollis orci. Maecenas ac volutpat massa. Nunc et ligula accumsan, condimentum lectus nec, fringilla nisl.
+					</p>	
+				</div>
+				<div class="buttons">
+				<div class="error">
+					<img class="knapp" src="Images/error.png">
+					
+				</div>
+				<div class="edit">
+					<img class="knapp" src="Images/edit.png">
+					
+				</div>
+			</div>
+			</div>
+			
+						
+
+
+
+			</div> <!-- blackc ends-->
+
+			<img src="Images/tri3.png" class="tri2"> <!-- triangle -->
+			
+			
+
+		</div> <!-- content div ends -->
+
+		<script> 
+			
+		</script>
+		
 	</body>
 </html>
