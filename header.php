1879d06e4c4e76638542e1485afeec93e9f19fe0
annadarner <darner.96@hotmail.com>
2017-11-02 17:30:05 +0100

    ändrade menyn

--- header.php
+++ header.php
@@ -45,8 +45,7 @@
 					<li><a class="<?php echo ($current_page == 'index.php' || $current_page == '') ? 'active' : NULL ?>" href="index.php"> Home</a></li>
 					<li><a class="<?php echo $current_page == 'finddrinks.php' ? 'active' : NULL ?>" href="finddrinks.php">Find drinks</a></li>
 					<li><a class="<?php echo $current_page == 'mydrinks.php' ? 'active' : NULL ?>" href="mydrinks.php">My drinks</a></li>
-					<li><a class="<?php echo $current_page == 'mybooks.php' ? 'active' : NULL ?>" href="mybooks.php">Inspo</a></li>
-					<li><a class="<?php echo $current_page == 'gallery.php' ? 'active' : NULL ?>" href="gallery.php">About us</a></li>
+					
 				</ul>
 			</nav>
 			
