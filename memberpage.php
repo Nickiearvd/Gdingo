5ebb05899588a6176efb3ea60a6dc88732c5f6a2
annadarner <darner.96@hotmail.com>
2017-11-10 18:17:29 +0100

    lala

--- memberpage.php
+++ memberpage.php
@@ -1,13 +1,13 @@
 <?php require('includes/config.php'); 
 
 //if not logged in redirect to login page
-if(!$user->is_logged_in()){ header('Location: login.php'); exit(); }
+if(!$user->is_logged_in()){ header('Location: mypanel.php'); exit(); }
 
 //define page title
 $title = 'Members Page';
 
 //include header template
-require('layout/header.php'); 
+require('layout/top.php'); 
 ?>
 
 <div class="container">
