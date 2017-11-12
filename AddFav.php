149530fe62c98ef846457914c1d28a7897fc51e5
Nickiearvd <arvidssonnickie@gmail.com>
2017-11-12 20:18:22 +0100

    bra

--- AddFav.php
+++ AddFav.php
@@ -1,7 +1,6 @@
 <!Doctype html>
 <?php require ("includes/config.php"); ?>
 <html>
-
     <head>
         <title>Index</title>
             <Meta name="description" content="my page"/>
@@ -10,62 +9,45 @@
             <link rel="stylesheet" href="removeFavorite.css" type="text/css" />
             <script type="text/javascript" src="jquery.min.js"></script>
             <script>
-
-                $(document).ready( function() {
-                    
+                $(document).ready( function() { 
                     function redirect(){
                         history.back();
                     }
-
                     setTimeout(redirect, 1000);
-
                 } );
-               
             </script>
     </head>
     <body>
-
         <div id="main">
             <div class="animation-target">
                 <img id="img" src="Images/checked.png"/>
                 <h1>ADDED</h1>
             </div>
         </div>
-
-    
         <?php
-include("config.php");
+            include("config.php");
+            $DrinkId = trim($_GET['DrinkId']);
+            echo '<INPUT type="hidden" name="drinkid" value=' . $DrinkId . '>';
 
+            $DrinkId = trim($_GET['DrinkId']);      // From the hidden field
+            $DrinkId = addslashes($DrinkId);
+            $User=($_SESSION['username']);
 
+            @ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
 
-$DrinkId = trim($_GET['DrinkId']);
-echo '<INPUT type="hidden" name="drinkid" value=' . $DrinkId . '>';
+            if ($db->connect_error) {
+                echo "could not connect: " . $db->connect_error;
+                printf("<br><a href=index.php>Return to home page </a>");
+                exit();
+            }
 
-$DrinkId = trim($_GET['DrinkId']);      // From the hidden field
-$DrinkId = addslashes($DrinkId);
-$User=($_SESSION['username']);
-
-
-
-
-@ $db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
-
-    if ($db->connect_error) {
-        echo "could not connect: " . $db->connect_error;
-        printf("<br><a href=index.php>Return to home page </a>");
-        exit();
-    }
-    // Prepare an insert statement with the current drinkID and userID and execute it
-    $stmt = $db->prepare("INSERT INTO Favo(DrinkId, username) VALUES (?,?)");
-    $stmt->bind_param('is', $DrinkId, $User);
-    $stmt->execute();
-    
-    exit;
-
-?>
-
-
-        
+            // Prepare an insert statement with the current drinkID and userID and execute it
+            $stmt = $db->prepare("INSERT INTO Favo(DrinkId, username) VALUES (?,?)");
+            $stmt->bind_param('is', $DrinkId, $User);
+            $stmt->execute();
+            
+            exit;
+        ?>
     </body>
 </html>
 
